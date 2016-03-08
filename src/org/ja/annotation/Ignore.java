7a0fcf7193e8ed06030d32009694a1531bce0d48
Manikanta <manikanta@egovernments.org>
2016-03-08 14:42:46 +0530

    adding ignore annotation

---
+++ src/org/ja/annotation/Ignore.java
@@ -0,0 +1,18 @@
+package org.ja.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+/**
+ * 
+ * @author mani
+ * Annotion to mark a field to be 
+ * ignored for jsp,controller creation
+ * 
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface Ignore {
+
+}
